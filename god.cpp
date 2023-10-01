03086f33be2cbcf99448a18693877e03ba8c9af3
Jessé Costa <jesse_cost@hotmail.com>
2023-10-01 17:54:31 -0300

    modifica

--- god.cpp
+++ god.cpp
@@ -1,61 +1,92 @@
-class Deuses {
-public:
-    Deuses(std::string nome = "Kratos", int vida = 100, double moedas = 0.0, int dano = 200, double poder = 500.0)
-        : nome(nome), vida(vida), moedas(moedas), dano(dano), poder(poder) {}
+#include <iostream>
+#include <string>
+#include "god.h"
 
-    Deuses(std::string nome, int vida, double moedas)
-        : nome(nome), vida(vida), moedas(moedas), dano(200), poder(500.0) {}
+using namespace std;
 
-    // Getters e setters
-    std::string getNome() const { return nome; }
+std::string getNome() const { 
+        return nome; }
+    
     int getVida() const { return vida; }
-    void setVida(int novaVida) { vida = novaVida; }
-    double getMoedas() const { return moedas; }
-    void setMoedas(double novasMoedas) { moedas = novasMoedas; }
-    int getDano() const { return dano; }
-    double getPoder() const { return poder; }
+    
+    void setVida(int novaVida) { 
+        vida = novaVida; }
+    
+    double getMoedas() const {
+         return moedas; }
+    
+    void setMoedas(double novasMoedas) { 
+        moedas = novasMoedas; }
+   
+    int getDano() const { 
+        return dano; }
+    
+    double getPoder() const { 
+        return poder; }
 
-private:
-    std::string nome;
-    int vida;
-    double moedas;
-    int dano;
-    double poder;
-};
+    // Classe utensilios
 
-class Utensilios {
-public:
-    Utensilios(std::string nome, int dano, double preco, int nivelJogo)
-        : nome(nome), dano(dano), preco(preco), nivelJogo(nivelJogo) {}
+    std::string getNome() const { 
+        return nome; 
+        }
+    
+    int getDano() const { 
+        return dano; 
+        }
+    
+    double getPreco() const { 
+        return preco; 
+        }
+    
+    int getNivelJogo() const { 
+        return nivelJogo; 
+        }
 
-    // Getters
-    std::string getNome() const { return nome; }
-    int getDano() const { return dano; }
-    double getPreco() const { return preco; }
-    int getNivelJogo() const { return nivelJogo; }
+    // Classe Jornada
 
-private:
-    std::string nome;
-    int dano;
-    double preco;
-    int nivelJogo;
-};
-
-class Jornada {
-public:
-    Jornada(std::string informacoes, std::string historia)
-        : informacoes(informacoes), historia(historia) {}
-
-    // Método constante
-    void imprimirInformacoes() const {
+     void imprimirInformacoes() const {
         std::cout << informacoes << std::endl;
     }
 
-    // Getters
-    std::string getInformacoes() const { return informacoes; }
-    std::string getHistoria() const { return historia; }
+    // Classe Deuses (2)
 
-private:
-    std::string informacoes;
-    std::string historia;
-};
+    std::string getNome() const { 
+        return nome; }
+    
+    int getVida() const { 
+        return vida; }
+    
+    void setVida(int novaVida) { 
+        vida = novaVida; }
+    
+    double getMoedas() const { 
+        return moedas; }
+    
+    void setMoedas(double novasMoedas) { 
+        moedas = novasMoedas; }
+    
+    int getDano() const { 
+        return dano; }
+    
+    double getPoder() const { 
+        return poder; }
+
+     // Classe utensilios (2)
+
+     std::string getNome() const { 
+        return nome; 
+        }
+    
+    int getDano() const { 
+        return dano; 
+        }
+    
+    double getPreco() const { 
+        return preco; 
+        }
+    
+    int getNivelJogo() const { 
+        return nivelJogo; 
+        } 
+        
+          
